df73172d2feae22d534f18bcd119fa3711dc9d95
Okonnah Victoria <victoriaokonnah@gmail.com>
2023-01-28 05:35:36 -0600

    footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom'
 const Footer = () => {
   return (
     <div className='bg-[#322F2F] text-white'>
-        <div className='w-[70%] mx-auto py-[1.5rem] grid gap-[2rem]'>
+        <div className='w-[80%] mx-auto py-[1.5rem] grid gap-[2rem]'>
             <div className='md:w-[85%] md:mx-auto '>
                 <label htmlFor="news" className='hidden'>Sign up for updated from dukia gold.</label>
                 <p>Sign up for updated from dukia gold.</p>
@@ -20,7 +20,7 @@ const Footer = () => {
                     <Link>Terms & Conditions</Link>
                     <Link>Data Privacy</Link>
                 </div>
-                <div className='flex w-[7rem] justify-between'>
+                <div className='flex w-[6rem] justify-between'>
                     <Link><FaTwitter size='1.3rem'/></Link>
                     <Link><FaInstagram size='1.3rem'/></Link>
                     <Link><FaFacebook size='1.3rem'/></Link>
